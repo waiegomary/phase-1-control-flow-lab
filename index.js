f5a3d027abe2c6b8372d1f3dcaf4813de2941568
mary_waiyego <waiyegomary55@gmail.com>
2023-03-13 19:02:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function ternaryCheckCity(){
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+}
